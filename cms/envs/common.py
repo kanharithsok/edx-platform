bdb548b306dd8ae5e0f1f2b7f4952fdfc11835b5
Kanharith Sok <kanharithsok@gmail.com>
2024-11-24 22:43:57 +0700

    Update MAX_ASSET_UPLOAD_FILE_SIZE_IN_MB

--- cms/envs/common.py
+++ cms/envs/common.py
@@ -2021,10 +2021,10 @@ DATA_UPLOAD_MAX_MEMORY_SIZE = None
 DATA_UPLOAD_MAX_NUMBER_FIELDS = None
 
 ### Size of chunks into which asset uploads will be divided
-UPLOAD_CHUNK_SIZE_IN_MB = 10
+UPLOAD_CHUNK_SIZE_IN_MB = 1024
 
 ### Max size of asset uploads to GridFS
-MAX_ASSET_UPLOAD_FILE_SIZE_IN_MB = 20
+MAX_ASSET_UPLOAD_FILE_SIZE_IN_MB = 1024
 
 # FAQ url to direct users to if they upload
 # a file that exceeds the above size
